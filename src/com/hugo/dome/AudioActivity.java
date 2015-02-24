49dba22f45d442e70125c667e2791cc416a4dac4
hugo porta <h.porta@orange.fr>
2015-02-24 13:45:42 +0100

    création de classe mpreview création de de la méthode save picture timer sur cette méthode coder fonctionnel

--- src/com/hugo/dome/AudioActivity.java
+++ src/com/hugo/dome/AudioActivity.java
@@ -2,23 +2,36 @@ package com.hugo.dome;
 
 import android.support.v7.app.ActionBarActivity;
 import android.view.animation.*;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AudioActivity extends ActionBarActivity {
 	ImageView logo = null;
-
+	Button button = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_audio);
+		button = (Button)findViewById(R.id.button);
 		logo = (ImageView)findViewById(R.id.logo);
 		Animation animation= AnimationUtils.loadAnimation(this,R.anim.rotation);
 		logo.startAnimation(animation);
+		
+		button.setOnClickListener(stopListener);
 	}
-
+OnClickListener stopListener =new OnClickListener(){
+	public void onClick(View v){
+		Intent Activite2 = new Intent(AudioActivity.this, MainActivity.class);
+		startActivity(Activite2);
+	}
+	
+};
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- src/com/hugo/dome/CameraActivity.java
+++ src/com/hugo/dome/CameraActivity.java
@@ -1,113 +1,114 @@
 package com.hugo.dome;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore.Images.Media;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 
 //Notre classe implémente SurfaceHolder.Callback
-public class CameraActivity extends Activity implements SurfaceHolder.Callback{
-	private Camera camera;
-	private SurfaceView surfaceCamera;
-	private Boolean isPreview;
+public class CameraActivity extends Activity {
+	private Handler myHandler;
+	private Runnable myRunnable = new Runnable(){
+		public void run() {
+			SavePicture();	    		
+			myHandler.postDelayed(this,60000);
+			}
+	};
+	private Preview mPreview;
+	FileOutputStream stream;
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getWindow().setFormat(PixelFormat.TRANSLUCENT);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-		isPreview = false;
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new Preview(this);
+        mPreview.setOnClickListener(photoListener);
+        setContentView(mPreview);
+        myHandler = new Handler();
+        myHandler.postDelayed(myRunnable,60000);
+    }
+    private OnClickListener photoListener = new OnClickListener(){
+    	public void onClick(View v) {
+    		// On prend une photo
+    		if (mPreview != null) {
+    			SavePicture();
+    		}
+    	}
+    };
+    private void SavePicture() {
+    	try {
+    		SimpleDateFormat timeStampFormat = new SimpleDateFormat(
+    				"yyyy-MM-dd-HH.mm.ss");
+    		String fileName = "photo_" + timeStampFormat.format(new Date())
+    				+ ".jpg";
 
-		// On applique notre layout
-		setContentView(R.layout.activity_camera);
+    		// Metadata pour la photo
+    		ContentValues values = new ContentValues();
+    		values.put(Media.TITLE, fileName);
+    		values.put(Media.DISPLAY_NAME, fileName);
+    		values.put(Media.DESCRIPTION, "Image prise par FormationCamera");
+    		values.put(Media.DATE_TAKEN, new Date().getTime());
+    		values.put(Media.MIME_TYPE, "image/jpeg");
 
-		// On récupère notre surface pour le preview
-		surfaceCamera = (SurfaceView) findViewById(R.id.surface_view);
+    		// Support de stockage
+    		Uri taken = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI,
+    				values);
 
-		// Méthode d'initialisation de la caméra
-		InitializeCamera();
-		/*surfaceCreated(surfaceCamera.getHolder());*/
-	}
+    		// Ouverture du flux pour la sauvegarde
+    		stream = (FileOutputStream) getContentResolver().openOutputStream(
+    				taken);
 
-	public void InitializeCamera() {
-		// On attache nos retour du holder à notre activite
-		surfaceCamera.getHolder().addCallback(this);
+    		mPreview.getCamera().takePicture(null, pictureCallback, pictureCallback);
+    	} catch (Exception e) {
+    		// TODO: handle exception
+    	}
 
-		// On spécifie le type du hoder en mode SURFACE_TYPE_PUSH_BUFFERS
-		surfaceCamera.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
-
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-
-		if (camera == null)
-			camera = Camera.open();
-	}
-
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-		// Si le mode preview est lancé alors on le stop
-		if (isPreview) {
-			camera.stopPreview();
-		}
-		// On récupère les parametres de la camera
-		Camera.Parameters parameters = camera.getParameters();
-
-		// On change la taille
-		parameters.setPreviewSize(width, height);
-
-		// On applique nos nouveaux parametres
-		camera.setParameters(parameters);
-		try {
-			// On attache notre previsualisation de la camera au holder de la
-			// surface
-			camera.setPreviewDisplay(surfaceCamera.getHolder());
-		} catch (IOException e) {
-		}
-
-		// On lance la previeuw
-		camera.startPreview();
-
-		isPreview = true;
-
-	}
-
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-
-		if (camera != null) {
-			camera.stopPreview();
-			isPreview = false;
-			camera.release();
-		}	
-	}
-	@Override
-	public void onResume() {
-		super.onResume();
-		camera = Camera.open();
-	}
-
-	// Mise en pause de l'application
-	@Override
-	public void onPause() {
-		super.onPause();
-
-		if (camera != null) {
-			camera.release();
-			camera = null;
-		}
-	}
+    }
+    Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
+    	 
+    	public void onPictureTaken(byte[] data, Camera camera) {
+    		if (data != null) {
+    			// Enregistrement de votre image
+    			try {
+    				if (stream != null) {
+    					stream.write(data);
+    					stream.flush();
+    					stream.close();
+    				}
+    			} catch (Exception e) {
+    				// TODO: handle exception
+    			}
+     
+    			// On redémarre la prévisualisation
+    			camera.startPreview();
+    		}
+    	}
+    };
+    public void onPause() {
+        super.onPause();
+        if(myHandler != null)
+            myHandler.removeCallbacks(myRunnable); // On arrete le callback
+    }
 }
+	
