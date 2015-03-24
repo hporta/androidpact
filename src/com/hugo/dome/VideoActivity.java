0e567bb38cf0befc9aa811148a56c6829d52c27f
hugo porta <h.porta@orange.fr>
2015-03-24 16:45:50 +0100

    chargement de l'interface graphique et création des attributs et constantes de classe

--- src/com/hugo/dome/VideoActivity.java
+++ src/com/hugo/dome/VideoActivity.java
@@ -1,16 +1,34 @@
 package com.hugo.dome;
 
 import android.support.v7.app.ActionBarActivity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.widget.Button;
+import android.widget.VideoView;
 
-public class VideoActivity extends ActionBarActivity {
+public class VideoActivity extends ActionBarActivity implements SurfaceHolder.Callback {
+	private MediaRecorder recorder = null;
+	private static final String OUTPUT_FILE = "/sdcard/videorecording.mp4";
+	private static final String Tag ="VideoCam";
+	private VideoView videoView = null;
+	private Button start= null;
+	private Button record = null;
+	private Boolean playing = false;
+	private Boolean recording =false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_video);
+		start = (Button) findViewById(R.id.bgnBtn);
+		record = (Button) findViewById(R.id.playRecordingBtn);
+		videoView= (VideoView) findViewById(R.id.videoView);
+		final SurfaceHolder holder = videoView.getHolder();
+		holder.addCallback(this);
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
 
 	@Override
