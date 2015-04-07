4e5fe5ffff867250ddf2af4292c1286eaf5ebce1
hugo porta <h.porta@orange.fr>
2015-04-07 14:11:48 +0200

    début du listener start

--- src/com/hugo/dome/VideoActivity.java
+++ src/com/hugo/dome/VideoActivity.java
@@ -3,6 +3,7 @@ package com.hugo.dome;
 import android.support.v7.app.ActionBarActivity;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
@@ -14,7 +15,7 @@ import android.widget.VideoView;
 public class VideoActivity extends ActionBarActivity implements SurfaceHolder.Callback {
 	private MediaRecorder recorder = null;
 	private static final String OUTPUT_FILE = "/sdcard/videorecording.mp4";
-	private static final String Tag ="VideoCam";
+	private static final String Tag ="VideoActivity";
 	private VideoView videoView = null;
 	private Button start= null;
 	private Button record = null;
@@ -32,17 +33,29 @@ public class VideoActivity extends ActionBarActivity implements SurfaceHolder.Ca
 		holder.addCallback(this);
 		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		
-		start.setOnClickListener(startlistener);
+		start.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				if(!VideoActivity.this.recording & !VideoActivity.this.playing){
+					try{
+						beginRecording(holder);
+						playing = false;
+						recording = true;
+						start.setBackgroundResource(R.drawable.stop);
+					}catch (Exception e ){
+					Log.e(Tag, e.toString());
+					e.printStackTrace();
+					}
+				}
+				else if(VideoActivity.this.recording){
+					try
+					
+				}
+			}
+		});
 		record.setOnClickListener(recordlistener);
 		
 	}
-OnClickListener startlistener = new View.OnClickListener(){
-	public void onClick(View v){
-		
-	}
-	
-	
-};
+
 OnClickListener recordlistener = new View.OnClickListener(){
 public void onClick(View v){
 		
